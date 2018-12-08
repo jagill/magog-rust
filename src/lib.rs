9e36329ff86569494ed9ecccdedba240a269e53d
James A. Gill <jagill@fb.com>
2018-12-08 14:02:36 -0500

    Temporary comment out unfinished relation and property mods

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ extern crate num_traits;
 mod types;
 pub use crate::types::*;
 
-mod relation;
-pub use crate::relation::*;
-
-mod properties;
-pub use crate::properties::*;
+// mod relation;
+// pub use crate::relation::*;
+//
+// mod properties;
+// pub use crate::properties::*;
 
 mod utils;
 
