54321b44d4af60ada875536038c79e04b02c1b4a
James A. Gill <jagill@fb.com>
2019-03-24 11:22:23 -0400

    Also rename test cases.

--- src/types/primitive/coordinate.rs
+++ src/types/primitive/coordinate.rs
@@ -120,7 +120,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_basic_coordinate_f32() {
+    fn check_basic_coord2_f32() {
         let x: f32 = 1.;
         let y: f32 = 2.;
         let c = Coord2 { x: x, y: y };
@@ -129,7 +129,7 @@ mod tests {
     }
 
     #[test]
-    fn check_basic_coordinate_f64() {
+    fn check_basic_coord2_f64() {
         let x: f64 = 1.;
         let y: f64 = 2.;
         let c = Coord2 { x: x, y: y };
@@ -138,21 +138,21 @@ mod tests {
     }
 
     #[test]
-    fn check_coordinate_equals() {
+    fn check_coord2_equals() {
         let c1 = Coord2 { x: 1., y: 2. };
         let c2 = Coord2 { x: 1., y: 2. };
         assert_eq!(c1, c2);
     }
 
     #[test]
-    fn check_coordinate_not_equals() {
+    fn check_coord2_not_equals() {
         let c1 = Coord2 { x: 1., y: 2. };
         let c2 = Coord2 { x: 2., y: 1. };
         assert_ne!(c1, c2);
     }
 
     #[test]
-    fn check_new_coordinate_f32() {
+    fn check_new_coord2_f32() {
         let x: f32 = 1.;
         let y: f32 = 2.;
         let c = Coord2::new(x, y);
@@ -161,7 +161,7 @@ mod tests {
     }
 
     #[test]
-    fn check_new_coordinate_f64() {
+    fn check_new_coord2_f64() {
         let x: f64 = 1.;
         let y: f64 = 2.;
         let c = Coord2::new(x, y);
